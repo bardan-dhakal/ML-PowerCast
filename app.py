2aa714aec41f093d998ac73b7285a68b07df6689
Bardan Dhakal <bardandhakal2@gmail.com>
2024-12-23 14:23:30 -0600

    Added git ignore and requirements.txt

--- app.py
+++ app.py
@@ -2,6 +2,9 @@ import streamlit as st
 import pandas as pd
 import pickle
 
+
+
+# Load the trained model
 def load_model():
     with open('energy_consumption_model.pkl', 'rb') as file:
         data = pickle.load(file)
@@ -9,5 +12,8 @@ def load_model():
 
 data = load_model()
 
+
+
+# App title
 st.title("XGBoost Regression Model for predicting Energy Consumption")
 st.write("This web application predicts the energy consumption of various regions in the United States using a trained XGBoost regression model.")
