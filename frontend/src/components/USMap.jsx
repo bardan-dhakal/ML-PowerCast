82666feb1b91bf086379408baf3c9669d755eb81
Bardan Dhakal <bardandhakal2@gmail.com>
2025-03-06 01:35:54 -0600

    Added Region wise clicking on US map

--- frontend/src/components/USMap.jsx
+++ frontend/src/components/USMap.jsx
@@ -2,7 +2,30 @@ import { ComposableMap, Geographies, Geography } from "react-simple-maps"
 
 const geoUrl = "https://cdn.jsdelivr.net/npm/us-atlas@3/states-10m.json"
 
+const pjmStates = [
+    "Delaware",
+    "Illinois",
+    "Indiana",
+    "Kentucky",
+    "Maryland",
+    "Michigan",
+    "New Jersey",
+    "North Carolina",
+    "Ohio",
+    "Pennsylvania",
+    "Tennessee",
+    "Virginia",
+    "West Virginia",
+    "District of Columbia"
+  ]
+
 const USMap = () => {
+
+    const handleRegionClick = (geo) => {
+        if (pjmStates.includes(geo.properties.name)){
+            console.log("Clicked PJM state")
+        }
+    }
     return (
 <div style={{ width: "100%", height: "500px" }}>
       <ComposableMap projection="geoAlbersUsa">
@@ -16,9 +39,12 @@ const USMap = () => {
                 stroke="#FFF"
                 style={{
                   default: { outline: "none" },
-                  hover: { fill: "#999", outline: "none" },
+                  hover: { fill: isPJMState ? "#41ae76" : "#999", 
+                    outline: "none" },
                   pressed: { outline: "none" }
+
                 }}
+                onClick = {() => handleRegionClick(geo)}
               />
             ))
           }
